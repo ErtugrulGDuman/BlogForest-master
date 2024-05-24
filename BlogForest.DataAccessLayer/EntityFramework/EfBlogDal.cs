0c2df9ce9d3fd71d3579032917fe5486ac50bd96
Ertuğrul Gökay Duman <ertugrulgokayduman@gmail.com>
2024-05-24 22:19:24 +0300

    ...

--- BlogForest.DataAccessLayer/EntityFramework/EfBlogDal.cs
+++ BlogForest.DataAccessLayer/EntityFramework/EfBlogDal.cs
@@ -19,7 +19,15 @@ namespace BlogForest.DataAccessLayer.EntityFramework
         public List<Blog> GetBlogsWithCategoryAndUser()
         {
             var context = new BlogContext();
-            var values = context.Blogs.Include(x=>x.Category).Include(y=>y.AppUser).ToList();
+            var values = context.Blogs.Include(x => x.Category).Include(y => y.AppUser).ToList();
+            return values;
+        }
+
+        public List<Blog> GetLast2BlogByAppUser(int id)
+        {
+            var context = new BlogContext();
+            int appUserId = context.Blogs.Where(x => x.BlogId == id).Select(y => y.AppUserId).FirstOrDefault();
+            var values = context.Blogs.Where(x => x.AppUserId == appUserId).OrderByDescending(y => y.BlogId).Take(2).ToList();
             return values;
         }
     }
